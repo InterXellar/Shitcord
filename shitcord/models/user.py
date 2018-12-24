e6eda8f5dea451ab7ae7def8a880ac996c406439
Vale <38182450+itsVale@users.noreply.github.com>
2018-12-24 21:31:39 +0100

    Some changes here and there + documentation

--- shitcord/models/user.py
+++ shitcord/models/user.py
@@ -4,7 +4,7 @@ import enum
 
 from . import abc
 from .base import Model
-from .errors import NoFlags
+from .errors import MissingProfile
 from ..utils import cdn
 
 __all__ = ['User']
@@ -35,10 +35,26 @@ class Flags(enum.IntEnum):
 
 
 class _BaseUser(Model, abc.DiscordUser):
+    """Represents a base class for Users on Discord.
+
+    It implements some core functionality Users as well as Members have.
+
+    Attributes
+    ----------
+    snowflake : shitcord.models.Snowflake
+        A `Snowflake` object that represents the user's ID.
+    id : int
+        The user's ID.
+    name : str
+        The user's name.
+    avatar_hash : str
+        The hash of the user's avatar.
+    """
+
     def __init__(self, data, http):
         super().__init__(data['id'], http=http)
 
-        self.username = data['username']
+        self.name = data['username']
         self.discriminator = int(data['discriminator'])
         self.avatar_hash = data['avatar']
 
@@ -51,33 +67,52 @@ class _BaseUser(Model, abc.DiscordUser):
 
     @property
     def default_avatar(self):
+        """Returns an enum member that represents the user's default avatar."""
+
         return cdn.PlebAvatar(self.discriminator % 5)
 
     @property
     def avatar(self):
+        """Returns a CDN url for the user's avatar."""
+
         return self.avatar_url()
 
     @property
     def animated_avatar(self):
+        """Returns a boolean indicating whether the user's avatar is animated or not."""
+
         avatar_hash = self.avatar_hash
         return avatar_hash and avatar_hash.startswith('a_')
 
     @property
     def default_avatar_url(self):
+        """Returns a CDN url for the user's default avatar."""
+
         return cdn.format_url(cdn.Endpoints.DEFAULT_USER_AVATAR, dict(discriminator=self.default_avatar.value), image_format='png')
 
-    def avatar_url(self, fmt='webp', size=1024):
+    def avatar_url(self, image_format=None, size=1024):
+        """Returns a CDN url for the user's avatar.
+
+        Parameters
+        ----------
+        image_format : str
+            The file format for the user's avatar. Defaults to 'webp'.
+        size : int, optional
+            The size of the image from the CDN url. Defaults to '1024'. Must be in a range between 16 and 2048.
+        """
+
         if not self.avatar_hash:
             return self.get_default_avatar()
 
-        if self.animated_avatar:
-            # For the case of an animated avatar, always force the CDN URL to represent the gif format.
-            fmt = 'gif'
-            animated = True
-        else:
-            animated = False
+        if image_format is None:
+            image_format = 'webp'
+            if self.animated_avatar:
+                image_format = 'gif'
 
-        return cdn.format_url(cdn.Endpoints.USER_AVATAR, dict(user=self.id, hash=self.avatar_hash), image_format=fmt, size=size, animated=animated)
+        animated = image_format == 'gif'
+
+        return cdn.format_url(cdn.Endpoints.USER_AVATAR,
+                              dict(user=self.id, hash=self.avatar_hash), image_format=image_format, size=size, animated=animated)
 
 
 class User(_BaseUser):
@@ -94,41 +129,63 @@ class User(_BaseUser):
 
     @property
     def mention(self):
+        """Returns a string that mentions the user."""
+
         return '<@{}>'.format(self.id)
 
     def _has_flag(self, flag):
         if self.flags is None:
-            raise NoFlags('User object has no flags attached.')
+            raise MissingProfile('User object has no flags attached.')
 
         value = flag.value
         return (self.flags & value) == value
 
     @property
     def nitro_classic(self):
-        return self.premium_type is not None and self.premium_type == 1
+        """Indicates whether the user has Nitro Classic or not."""
+
+        if self.premium_type is None:
+            raise MissingProfile('No information about Nitro status are accessible.')
+
+        return self.premium_type == 1
 
     @property
     def nitro(self):
-        return self.premium_type is not None and self.premium_type == 2
+        """Indicates whether the user has Nitro or not."""
+
+        if self.premium_type is None:
+            raise MissingProfile('No information about Nitro status are accessible.')
+
+        return self.premium_type == 2
 
     @property
     def employee(self):
+        """Indicates whether the user is a Discord Employee or not."""
+
         return self._has_flag(Flags.DISCORD_EMPLOYEE)
 
     @property
     def partner(self):
+        """Indicates whether the user is a Discord Partner or not."""
+
         return self._has_flag(Flags.DISCORD_PARTNER)
 
     @property
     def hypesquad(self):
+        """Indicates whether the user is inside HypeSquad Events or not."""
+
         return self._has_flag(Flags.DISCORD_HYPESQUAD)
 
     @property
     def bug_hunter(self):
+        """Indicates whether the user is a Bug Hunter or not."""
+
         return self._has_flag(Flags.DISCORD_BUG_HUNTER)
 
     @property
     def hypesquad_house(self):
+        """Returns a list of HypeSquad houses this user is in."""
+
         houses = [
             house for house, flag in zip(HypeSquadHouse, (Flags.HYPESQUAD_BRAVERY, Flags.HYPESQUAD_BRILLIANCE, Flags.HYPESQUAD_BALANCE))
             if self._has_flag(flag)
