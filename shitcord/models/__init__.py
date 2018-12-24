58596b84e349fc5d71ba7af0839faa56a0e13859
Vale <38182450+itsVale@users.noreply.github.com>
2018-12-24 21:32:13 +0100

    Updating some invalid stuff + adding the recent models

--- shitcord/models/__init__.py
+++ shitcord/models/__init__.py
@@ -11,9 +11,12 @@ from .base import Model
 from .channel import *
 from .colour import Color, Colour
 from .emoji import Emoji, PartialEmoji
-from .errors import ModelError, NoFlags
+from .errors import *
+from .permissions import Permissions, PermissionTypes
+from .role import Role
 from .snowflake import DISCORD_EPOCH, Snowflake
 from .user import User
 
-__all__ = ['Color', 'Colour', 'DISCORD_EPOCH', 'Emoji', 'PartialEmoji', 'TextChannel', 'DMChannel', 'Snowflake',
-           'VoiceChannel', 'GroupDMChannel', 'CategoryChannel', 'User']
+__all__ = ['Color', 'Colour', 'DISCORD_EPOCH', 'Emoji', 'PartialEmoji', 'Permissions',
+           'PermissionTypes', 'TextChannel', 'DMChannel', 'Role', 'Snowflake', 'VoiceChannel',
+           'GroupDMChannel', 'CategoryChannel', 'User']
