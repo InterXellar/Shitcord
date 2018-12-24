bd8b8db27f3d023f1e7b7448cee87a67f938de35
Vale <38182450+itsVale@users.noreply.github.com>
2018-12-24 21:29:06 +0100

    Adding documentation for Role

--- shitcord/models/role.py
+++ shitcord/models/role.py
@@ -6,7 +6,32 @@ from .permissions import Permissions
 
 
 class Role(Model):
-    __slots__ = ('_json', 'name', 'color', 'hoist', 'position', 'permissions', 'managed', 'mentionable')
+    """Represents a Role model from the Discord API.
+
+    A Role belongs to a Guild and is basically a set of permissions
+    attached to a group of users. They are unique per Guild.
+
+    Attributes
+    ----------
+    snowflake: shitcord.models.Snowflake
+        A `Snowflake` object that represents the role's ID.
+    id : int
+        The role's ID.
+    name : str
+        The role's name.
+    hoist: bool
+        A boolean indicating whether the role is hoisted or not.
+    position : int
+        The role's position in the hierarchy.
+    permissions : shitcord.models.Permissions
+        A `Permission` object representing the permissions attached to the role.
+    managed : bool
+        Whether the role is managed or not.
+    mentionable : bool
+        Whether the role is mentionable by everyone or not.
+    """
+
+    __slots__ = ('name', 'color', 'hoist', 'position', 'permissions', 'managed', 'mentionable')
 
     def __init__(self, data, http):
         super().__init__(data['id'], http=http)
@@ -27,4 +52,6 @@ class Role(Model):
 
     @property
     def mention(self):
+        """Returns a string that mentions the role."""
+
         return '<@&{}>'.format(self.id)
